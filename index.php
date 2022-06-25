d9a59b79d0e8c0bcb4b4806ca5a402b19c2806a6
Juan Benitez <jgbenitez10@misena.edu.co>
2022-06-24 22:20:57 -0500

    creacion del proyecto con sus carpetas correspondientes de la plantilla

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+    include('./view/header.html');
+?>
+
+<div class="container-fluid p-4">
+    <h2>hola desde index</h2>
+</div>
+
+<?php 
+    include('./view/footer.html');
+?>
---
+++ users.php
@@ -0,0 +1,11 @@
+<?php 
+    include('./view/header.html');
+?>
+
+<div class="container-fluid p-4">
+    <h2>hola desde usuarios</h2>
+</div>
+
+<?php 
+    include('./view/footer.html');
+?>
